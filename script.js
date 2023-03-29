2e92ead43b8901808ea828bcf6e7ec10d52c6517
sabrine21 <99128040+sabrine21@users.noreply.github.com>
2023-03-29 13:26:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let shoppingCart=document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick=() =>{
+    shoppingCart.classList.toggle('active');
+}
+let loginForm=document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick=() =>{
+    loginForm.classList.toggle('active');
+}
+
+let navbar=document.querySelector('.navbar');
+
+var swiper = new Swiper(".articles-ro", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
